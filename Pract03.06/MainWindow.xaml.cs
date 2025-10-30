caa3e7f84e6541a209d1e87b547ceeb1f8ea4257
ArtemkinM <artemkinm@gmail.com>
2025-10-30 21:23:21 +0300

    Добавьте файлы проекта.

---
+++ Pract03.06/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Pract03._06
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Money money1 = new Money();
+        Money money2 = new Money();
+        Money money3 = new Money();
+        int divider = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            tbDivider.Text = null;
+        }
+
+        private void Addition_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsSetMoneyForBinaryOperations())
+            {
+                money3 = money1 + money2;
+                tbRez.Text = Convert.ToString(money3.Rub + " руб., " + money3.Cop + " коп.");
+                tbDivider.Text = null;
+            }
+            else
+            {
+                MessageBox.Show("Введите значения!!!");
+                tbRez.Text = null;
+            }
+        }
+
+        private void Subtraction_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsSetMoneyForBinaryOperations())
+            {
+                money3 = Money.Subtraction(money1, money2);
+                tbRez.Text = Convert.ToString(money3.Rub + " руб., " + money3.Cop + " коп.");
+                tbDivider.Text = null;
+            }
+            else
+            {
+                MessageBox.Show("Введите значения!!!");
+                tbRez.Text = null;
+            }
+        }
+
+        private void Division_Click(object sender, RoutedEventArgs e)
+        {
+            tbCop2.Text = null;
+            tbRub2.Text = null;
+            if (IsSetMoneyForDivision())
+            {
+                money3 = Money.Division(money1, divider);
+                tbRez.Text = Convert.ToString(money3.Rub + " руб., " + money3.Cop + " коп.");
+            }
+            else
+            {
+                MessageBox.Show("Введите значения!!!");
+                tbRez.Text = null;
+            }
+        }
+
+        private void Info_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Создание наследованных классов.\nАртемкин М. ИСП-31 Практическая работа №6.\n\nИспользовать класс Pair (пара чисел). Определить класс-наследник Money с характеристиками: рубли и копейки.\nПереопределить операцию сложения и определить методы вычитания и деления денежных сумм.");
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private bool IsSetMoneyForDivision()
+        {
+            if (Int32.TryParse(tbRub1.Text, out int rub1) && rub1 >= 0 && Int32.TryParse(tbCop1.Text, out int cop1) && cop1 >= 0 && cop1 < 100 && Int32.TryParse(tbDivider.Text, out int dividerr) && dividerr > 0)
+            {
+                money1.Rub = rub1;
+                money1.Cop = cop1;
+                divider = dividerr;
+                return true;
+            }
+            else return false;
+        }
+
+        private bool IsSetMoneyForBinaryOperations()
+        {
+            if (Int32.TryParse(tbRub1.Text, out int rub1) && rub1 >= 0 && Int32.TryParse(tbCop1.Text, out int cop1) && cop1 >= 0 && cop1 < 100 && Int32.TryParse(tbRub2.Text, out int rub2) && rub2 >= 0 && Int32.TryParse(tbCop2.Text, out int cop2) && cop2 >= 0 && cop2 < 100)
+            {
+                money1.Rub = rub1;
+                money1.Cop = cop1;
+                money2.Rub = rub2;
+                money2.Cop = cop2;
+                return true;
+            }
+            else return false;
+        }
+    }
+}
---
+++ Pract03.06/PairM.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Effects;
+
+//Использовать класс Pair(пара чисел). Определить класс-наследник Money с характеристиками: 
+//рубли и копейки. Переопределить операцию сложения и определить методы вычитания и деления 
+//денежных сумм.
+
+namespace Pract03._06
+{
+    class Pair
+    {
+        public int A { get; set; }
+        public int B { get; set; }
+
+        /// <summary>
+        /// Пара чисел
+        /// </summary>
+        public Pair()
+        {
+            A = 1;
+            B = 1;
+        }
+        /// <summary>
+        /// Пара чисел с параметрами
+        /// </summary>
+        /// <param name="a">Параметр 1</param>
+        /// <param name="b">Параметр 2</param>
+        public Pair(int a, int b)
+        {
+            A = a;
+            B = b;
+        }
+
+        /// <summary>
+        /// Сложение пар
+        /// </summary>
+        /// <param name="pair1">Пара 1</param>
+        /// <param name="pair2">Пара 2</param>
+        /// <returns>Итоговая пара</returns>
+        public static Pair operator +(Pair pair1, Pair pair2)
+        {
+            Pair pair3 = new Pair();
+            pair3.A = pair1.A + pair2.A;
+            pair3.B = pair1.B + pair2.B;
+            return pair3;
+        }
+
+        /// <summary>
+        /// Плюс 1 к паре
+        /// </summary>
+        /// <param name="pair1">Пара</param>
+        /// <returns>Пара</returns>
+        public static Pair operator ++(Pair pair1)
+        {
+            pair1.B++;
+            pair1.A++;
+            return pair1;
+        }
+
+        /// <summary>
+        /// Минус 1 к паре
+        /// </summary>
+        /// <param name="pair1">Пара</param>
+        /// <returns>Пара</returns>
+        public static Pair operator --(Pair pair1)
+        {
+            pair1.A--;
+            pair1.B--;
+            return pair1;
+        }
+    }
+
+    class Money : Pair 
+    { 
+        public int Rub { get; set; }
+        public int Cop { get; set; }
+
+        public Money()
+        {
+            Rub = 0;
+            Cop = 0;
+        }
+
+        public Money(int rub, int cop)
+        {
+            Rub = rub;
+            Cop = cop;
+            if (cop >= 100)
+            {
+                Rub += cop / 100;
+                Cop = cop % 100;
+            }
+        }
+
+
+        public static Money operator +(Money money2, Money money1)
+        {
+            Money money3 = new Money();
+            money3.Rub = money1.Rub + money2.Rub;
+            money3.Cop = money1.Cop + money2.Cop;
+            if (money3.Cop >= 100)
+            {
+                money3.Rub += money3.Cop / 100;
+                money3.Cop = money3.Cop % 100;
+            }
+            return money3;
+        }
+
+        static public Money Subtraction (Money money1, Money money2)
+        {
+            Money money3 = new Money();
+            int totalCop1 = money1.Rub * 100 + money1.Cop;
+            int totalCop2 = money2.Rub * 100 + money2.Cop;
+
+            if (totalCop1 < totalCop2)
+            {
+                MessageBox.Show("!!! Нельзя вычитать большую сумму из меньшей !!!");
+                return money3;
+            }
+            else
+            {
+                int resultCop = totalCop1 - totalCop2;
+                money3.Rub = resultCop / 100;
+                money3.Cop = resultCop % 100;
+                return money3;
+            }
+        }
+
+        static public Money Division (Money money1, int divider)
+        {
+            Money money2 = new Money();
+            int cop = money1.Rub * 100 + money1.Cop;
+            int quotient = cop / divider;
+            money2.Cop = quotient % 100;
+            money2.Rub = quotient / 100;
+            return money2;
+        }
+    }
+}
